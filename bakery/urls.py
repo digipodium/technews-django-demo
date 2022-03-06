f9886ff402366acabb1f639d2526d3ae26b15d27
zaid kamil <xaidmetamorphos@gmail.com>
2022-03-06 16:02:24 +0530

    inheritance

--- bakery/urls.py
+++ bakery/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from .views import *
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('', bakery_menu, name='bakery_menu'),
+    path('about', TemplateView.as_view(template_name='about.html'), name='about'),
+    path('contact', TemplateView.as_view(template_name='contact.html'), name='contact'),
+    path('news', TemplateView.as_view(template_name='news.html'), name='news'),
 ]
